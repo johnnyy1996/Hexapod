13c12dd9773e632cb3213059d48886799b9b7e90
vc809 <thedominican_787@hotmail.com>
2016-03-18 09:10:01 -0400

    Mandibles open and close initial upload

---
+++ Constantz_Mandibles_open_close.ino
@@ -0,0 +1,37 @@
+/* 
+Victor Cuevas
+This code simply makes the robot open and close its mandibles
+*/
+
+void setup() 
+{
+  Serial.begin(115200);     // begin communication using 115.2K baud rate
+  while(!Serial)
+  {
+    ;                       // wait while connecting
+  }
+}
+
+void openMouth() //sends 2 servos comands to make mandibles open
+{
+  Serial.print ("#28P1200 #29P1700");
+  Serial.println();
+  delay(1000);
+}
+
+
+void closeMouth() //sends 2 servos comands to make mandibles close
+{
+  Serial.print ("#28P1700 #29P1200");
+  Serial.println();
+  delay(1000);
+}
+
+
+
+void loop() 
+{
+  openMouth();
+  closeMouth();
+
+}
