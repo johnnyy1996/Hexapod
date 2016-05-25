e7cefe95ab9283703ebbfe04a29850e185219cc2
vc809 <thedominican_787@hotmail.com>
2016-05-25 15:28:09 -0400

    Added comments and description of the program

--- Constantz_Mandibles_open_close.ino
+++ Constantz_Mandibles_open_close.ino
@@ -1,10 +1,11 @@
 /* 
 Victor Cuevas
 This code simply makes the robot open and close its mandibles
+This code was ment to take 4 inputs, 2 per jaw in order to fully open or fully close with slow and fast speed, but the architecture was abandoned.
 */
 
 int inbyte = 0;
-double lJaw [2] {0,0}; //#29 {F, S} 1700 fully open 1200 fully closed
+double lJaw [2] {0,0}; //#29 {F, S} 1700 fully open 1200 fully closed. F stands for fast and S for slow, an input of 1 on either of these will trigger appropiate response for the mandibles
 double rJaw [2] {0,0}; //#28 {F, S} 1200 fully open 1700 fully closed
 
 void setup() 
@@ -64,7 +65,7 @@ void jaws (double lJaw [], double rJaw [])
 }
 
 void loop() 
-{
+{//this is hardcoded input testing the code
   Serial.print("0");
   Serial.println();
   lJaw [0] = 0;
@@ -102,5 +103,6 @@ void loop()
   rJaw [1] = 0.8;
   jaws (lJaw, rJaw);
   delay(5000);
+  
 
 }
