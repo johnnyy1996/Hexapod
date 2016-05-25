7db3f70c887a61a69f15b858811997e1762beaac
vc809 <thedominican_787@hotmail.com>
2016-05-25 15:32:44 -0400

    added comments and description
    Also added a delay loop that will read input form the sensors while the movement is being executed

--- Constantz_Touch_Sensor.ino
+++ Constantz_Touch_Sensor.ino
@@ -1,6 +1,12 @@
-int valL = 0, valR = 0, counter = 0;
-unsigned long time;
-bool closed = 1;
+/* Author Victor Cuevas
+ *  This code uses pin 5 and pin 10 to read input signals from installed pressured sensors.
+ *  It displays the signal of the sensors every 10(+-1) miliseconds during mandibles movement
+  */
+
+int valL = 0, valR = 0; //valL and valR hold the values of the sensor to ben printed
+int counter = 0;  //the variable counter will be used to determine the amount of trials
+unsigned long time;  //will hold the time a comand was started in order to determine length of comand
+bool closed = 1;    //hold bool value wether the mandibles are open or not
 void setup() 
 {
   Serial.begin(115200);     // begin communication using 115.2K baud rate
@@ -8,32 +14,34 @@ void setup()
   {
     ;                       // wait while connecting
   }
-  Serial.print("#14P1500 #13P1577 #12P1500");
+  Serial.print("#14P1071 #13P1577 #12P1500");   //setup comands to position the head servos
   Serial.println();
   delay (3000);
 }
 
 void loop() 
 {
-  if (closed)
+  if (closed)                        //if mandibles are closed, comand to open
   {
     Serial.print("Open  ");
     Serial.print ("#28P1200 #29P1700");
     Serial.println();
     time = millis();
-    while ((millis() - time) < 500)
+    while ((millis() - time) < 500) //this acts as a delay in order for servos to complete the comand, in the meantime we read sensor inputs
+                                    //we give it a delay of 500 milliseconds
     {
-      Serial.print("time: ");
+      //Serial.print("time: ");
       Serial.print(millis()-time);
-      Serial.println();
-      valL = digitalRead(5);
-      valR = digitalRead(10);
+      Serial.print("\t");
+      valL = digitalRead(5);        //read left sensor signal
+      valR = digitalRead(10);       //read right sensor signal
       Serial.print(valL);
       Serial.print("\t");
       Serial.print(valR);
       Serial.println();
       Serial.println();
-      delay(10);
+      delay(10);                    //we read sensor signal every 10 milliseconds in order to not overflow with data
+                                    //we can read as fast as we can but keep in mind sensor data gets updated every 2-5 milliseconds
     }
     closed=0;
   }
@@ -45,9 +53,9 @@ void loop()
     time = millis();
     while ((millis() - time) < 500)
     {
-      Serial.print("time: ");
+      //Serial.print("time: ");
       Serial.print(millis()-time);
-      Serial.println();
+      Serial.print("\t");
       valL = digitalRead(5);
       valR = digitalRead(10);
       Serial.print(valL);
@@ -60,11 +68,11 @@ void loop()
     closed=1;
   }
   counter ++;
-  if (counter == 100)
+  if (counter == 100)               //this statement will check to see if given ammount of trials were completed
   {
     Serial.print ("trial Completed **********************");
     Serial.println();
-    while(counter ==100);
+    while(counter ==100);           //infinite loop to keep the robot from moving
     {
       ;
     }
