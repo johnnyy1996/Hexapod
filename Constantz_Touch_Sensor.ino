647ddc722b57f123be59da9119c1bfebeb1e8469
vc809 <thedominican_787@hotmail.com>
2016-05-26 10:28:49 -0400

    Added more comments

--- Constantz_Touch_Sensor.ino
+++ Constantz_Touch_Sensor.ino
@@ -1,12 +1,15 @@
 /* Author Victor Cuevas
- *  This code uses pin 5 and pin 10 to read input signals from installed pressured sensors.
- *  It displays the signal of the sensors every 10(+-1) miliseconds during mandibles movement
+ *  This code performs 100 test trials on the mandibles pressure sensors and displays the output signal on the screen
+ *  It uses pin 5 and pin 10 to read input signals from installed pressured sensors on the left and rigth mandibles.
+ *  It displays the signal of the sensors every 10(+-1) miliseconds with an aproximate delay of 500 milliseconds
+ *    for the movement to complete
+ *  An output of 1 or 0 would represent if the sensor is activated
   */
 
-int valL = 0, valR = 0; //valL and valR hold the values of the sensor to ben printed
-int counter = 0;  //the variable counter will be used to determine the amount of trials
-unsigned long time;  //will hold the time a comand was started in order to determine length of comand
-bool closed = 1;    //hold bool value wether the mandibles are open or not
+int valL = 0, valR = 0;     //valL and valR hold the values of the sensor to ben printed
+int counter = 0;            //the variable counter will be used to determine the amount of trials
+unsigned long time;         //will hold the time a comand was started in order to determine length of comand
+bool closed = 1;            //hold bool value wether the mandibles are open or not
 void setup() 
 {
   Serial.begin(115200);     // begin communication using 115.2K baud rate
@@ -21,16 +24,16 @@ void setup()
 
 void loop() 
 {
-  if (closed)                        //if mandibles are closed, comand to open
+  if (closed)                 //if mandibles are closed, comand to open
   {
     Serial.print("Open  ");
     Serial.print ("#28P1200 #29P1700");
     Serial.println();
-    time = millis();
-    while ((millis() - time) < 500) //this acts as a delay in order for servos to complete the comand, in the meantime we read sensor inputs
+    time = millis();          //record elapse time since program started, this would serve as tool to determine the delay we need
+    while ((millis() - time) < 500) //while the time elapse since comand has been give is < 500 milliseconds
+                                    //this acts as a delay in order for servos to complete the comand, in the meantime we read sensor inputs
                                     //we give it a delay of 500 milliseconds
     {
-      //Serial.print("time: ");
       Serial.print(millis()-time);
       Serial.print("\t");
       valL = digitalRead(5);        //read left sensor signal
